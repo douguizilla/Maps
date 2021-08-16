dc309598545d9fc609738bbb7f01c90025aeb617
douguizilla <douglasgommez@gmail.com>
2021-08-16 12:08:08 -0300

    refactored view binding

--- app/src/main/java/com/odougle/maps/MapActivity.kt
+++ app/src/main/java/com/odougle/maps/MapActivity.kt
@@ -10,11 +10,10 @@ class MapActivity : AppCompatActivity() {
         ActivityMapBinding.inflate(layoutInflater)
     }
 
-        private val TAG: String = "MapActivity"
+    private val TAG: String = "MapActivity"
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(binding.root)
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
     }
 }
