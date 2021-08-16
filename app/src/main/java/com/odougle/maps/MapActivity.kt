b604aa99edee90256e96073516394dc39e0571df
douguizilla <douglasgommez@gmail.com>
2021-08-16 11:51:18 -0300

    set binding

--- app/src/main/java/com/odougle/maps/MapActivity.kt
+++ app/src/main/java/com/odougle/maps/MapActivity.kt
@@ -2,10 +2,14 @@ package com.odougle.maps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.odougle.maps.databinding.ActivityMapBinding
 
 class MapActivity : AppCompatActivity() {
+
+    private val binding: ActivityMapBinding = ActivityMapBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        setContentView(binding.root)
     }
 }
